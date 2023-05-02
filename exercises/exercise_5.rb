c8545ec5594bff92d35d197d5cf485fbed4b8930
nelsonchengg <nelsoncheng103@gmail.com>
2023-05-02 02:20:11 -0400

    more queries

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue for this biz is $#{total_revenue}."
+
+average_annual_revenue = Store.average(:annual_revenue).round()
+puts "Average annual revenue is $#{average_annual_revenue}."
+
+generating_over_one_million = Store.where(annual_revenue: 1000000..Float::INFINITY).count
+puts "There are #{generating_over_one_million} stores making over $1 million."
