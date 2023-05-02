fc63094c930b21b6f31023668db101c57a8f5213
nelsonchengg <nelsoncheng103@gmail.com>
2023-05-02 03:00:14 -0400

    bug fix

--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
-  validates :name, presence: true, length: { minimum : 3 }
+  validates :name, presence: true, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :must_carry_apparel
 
